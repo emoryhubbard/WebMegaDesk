e541a1bf751db68902f649bfe59b3934e1a2743f
emoryhubbard <edhthe4th@gmail.com>
2023-06-11 04:14:57 -0400

    Added sort, search, and proper price calculation

--- WebMegaDesk/Pages/Create.cshtml.cs
+++ WebMegaDesk/Pages/Create.cshtml.cs
@@ -31,15 +31,82 @@ namespace WebMegaDesk.Pages
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || _context.DeskQuote == null || DeskQuote == null)
+            DeskQuote.Date = DateTime.Now;
+            DeskQuote.Price = GetTotal();
+            if (_context.DeskQuote == null || DeskQuote == null)
             {
                 return Page();
             }
-
             _context.DeskQuote.Add(DeskQuote);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
+        }
+        public int GetTotal() {
+            int total = 0;
+            int basePrice = 200;
+            int drawerPrice = 50;
+            int desktopArea = DeskQuote.Width * DeskQuote.Depth;
+
+            total += basePrice;
+            if (desktopArea > 1000)
+                total += desktopArea;
+            total += GetMaterialPrice();
+            total += DeskQuote.DrawerCount * drawerPrice;
+            total += GetRushPrice();
+
+            return total;
+        }
+        public int GetMaterialPrice() {
+            int total = 0;
+            int drawerPrice = 50;
+            int oakPrice = 200;
+            int laminatePrice = 100;
+            int pinePrice = 50;
+            int rosewoodPrice = 300;
+            int veneerPrice = 125;
+
+            total += DeskQuote.DrawerCount * drawerPrice;
+            if (DeskQuote.Material == "Oak")
+                total += oakPrice;
+            if (DeskQuote.Material == "Laminate")
+                total += laminatePrice;
+            if (DeskQuote.Material == "Pine")
+                total += pinePrice;
+            if (DeskQuote.Material == "Rosewood")
+                total += rosewoodPrice;
+            if (DeskQuote.Material == "Veneer")
+                total += veneerPrice;
+            return total;
+        }
+        public int GetRushPrice() {
+            int total = 0;
+            int desktopArea = DeskQuote.Width * DeskQuote.Depth;
+            if (DeskQuote.RushDays == 3) {
+                if (desktopArea < 1000)
+                    total += 60;
+                if (desktopArea >= 1000 && desktopArea <= 200)
+                    total += 70;
+                if (desktopArea > 2000)
+                    total += 80;
+            }
+            if (DeskQuote.RushDays == 5) {
+                if (desktopArea < 1000)
+                    total += 40;
+                if (desktopArea >= 1000 && desktopArea <= 200)
+                    total += 50;
+                if (desktopArea > 2000)
+                    total += 60;
+            }
+            if (DeskQuote.RushDays == 5) {
+                if (desktopArea < 1000)
+                    total += 30;
+                if (desktopArea >= 1000 && desktopArea <= 200)
+                    total += 35;
+                if (desktopArea > 2000)
+                    total += 40;
+            }
+            return total;
         }
     }
 }
--- WebMegaDesk/Pages/Index.cshtml.cs
+++ WebMegaDesk/Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebMegaDesk.Data;
 using WebMegaDesk.Models;
@@ -22,12 +23,49 @@ namespace WebMegaDesk.Pages
         
         public IList<DeskQuote> DeskQuote { get;set; } = default!;
 
+        public SelectList? Names { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? CustomerName { get; set; }
+        public SelectList? SortColumns { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? SortColumn { get; set; }
+
         public async Task OnGetAsync()
         {
             if (_context.DeskQuote != null)
             {
                 DeskQuote = await _context.DeskQuote.ToListAsync();
             }
+
+            // Use LINQ to get list of genres.
+            IQueryable<string> nameQuery = from d in _context.DeskQuote
+                                            orderby d.CustomerName
+                                            select d.CustomerName;
+
+            var deskQuotes = from d in _context.DeskQuote
+                             orderby d.CustomerName
+                         select d;
+
+            if (!string.IsNullOrEmpty(SortColumn)) {
+                deskQuotes = from d in _context.DeskQuote
+                             orderby d.Date
+                             select d;
+            }
+                /*
+                if (!string.IsNullOrEmpty(SearchString)) {
+                    deskQuotes = deskQuotes.Where(s => s.Title.Contains(SearchString));
+                }
+                */
+
+                if (!string.IsNullOrEmpty(CustomerName)) {
+                deskQuotes = (IOrderedQueryable<DeskQuote>) deskQuotes.Where(x => x.CustomerName == CustomerName);
+            }
+            Names = new SelectList(await nameQuery.Distinct().ToListAsync());
+
+            DeskQuote = await deskQuotes.ToListAsync();
+
         }
     }
 
